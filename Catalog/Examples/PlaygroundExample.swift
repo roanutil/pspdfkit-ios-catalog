35f560d6b3d8663ffe51e74b9ae6d09469f5c630
PSPDFKit <support@pspdfkit.com>
2023-04-27 12:20:02 +0200

    PSPDFKit for iOS Catalog example app

---
+++ Catalog/Examples/PlaygroundExample.swift
@@ -0,0 +1,35 @@
+//
+//  Copyright © 2017-2023 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+// See 'PSCObjectiveCExample.m' for the Objective-C version of this example.
+
+import PSPDFKit
+import PSPDFKitUI
+
+class PlaygroundExample: Example {
+
+    override init() {
+        super.init()
+
+        title = "Playground"
+        contentDescription = "Start here!"
+        category = .top
+        priority = 1
+    }
+
+    override func invoke(with delegate: ExampleRunnerDelegate) -> UIViewController {
+        // Playground is convenient for testing
+        let document = AssetLoader.writableDocument(for: .quickStart, overrideIfExists: false)
+
+        let controller = AdaptivePDFViewController(document: document) {
+            // Use the configuration to set main PSPDFKit options.
+            $0.signatureStore = KeychainSignatureStore()
+        }
+
+        return controller
+    }
+}
